8350273cd968b0ad985cafca9abf035558583017
suciutwo <andrewsuciu@gmail.com>
2014-12-09 00:48:13 -0800

    Fixed tests to run with new concept attribute.

--- kant/tests/test_concept.py
+++ kant/tests/test_concept.py
@@ -5,6 +5,6 @@ from kant import Concept
 class TestConcept(TestCase):
     def test_make(self):
         """ Creating a concept """
-        c = Concept("test concept", [Concept("sub concept")])
+        c = Concept("test concept", False, [Concept("sub concept", False)])
         self.assertTrue(isinstance(c, Concept))
-        self.assertTrue(c.contains(Concept("sub concept")))
+        self.assertTrue(c.contains(Concept("sub concept", False)))
--- kant/tests/test_judgment.py
+++ kant/tests/test_judgment.py
@@ -5,33 +5,33 @@ from kant import Concept, Judgment
 class TestJudgment(TestCase):
     def test_make(self):
         """ Creating a judgment """
-        c = Judgment(Concept("subject"), Concept("predicate"))
+        c = Judgment(Concept("subject", False), Concept("predicate", False))
         self.assertTrue(isinstance(c, Judgment))
         self.assertTrue(c.predicate.name == "predicate")
 
     def test_straight_line(self):
         """ pp145 """
-        straight = Concept("straight line")
-        shortest = Concept("shortest possible")
+        straight = Concept("straight line", "True")
+        shortest = Concept("shortest possible", "True")
         j = Judgment(straight, shortest)
         self.assertTrue(j.is_synthetic())
 
     def test_bodies(self):
         """ pp130 """
-        space = Concept("space", [Concept("extended")])
-        all_bodies = Concept("all bodies", [space])
-        extended = Concept("extended")
+        space = Concept("space", False, [Concept("extended", True)])
+        all_bodies = Concept("all bodies", False, [space])
+        extended = Concept("extended", True)
         j = Judgment(all_bodies, extended)
         self.assertTrue(j.is_analytic())
-        heavy = Concept("heavy")
+        heavy = Concept("heavy", True)
         j = Judgment(all_bodies, heavy)
         self.assertTrue(j.is_synthetic())
 
     def test_science(self):
         """ pp145 """
-        matter = Concept("matter", [Concept("presence")])
-        conserved = Concept("persistence")
+        matter = Concept("matter", True, [Concept("presence", True)])
+        conserved = Concept("persistence", True)
         all_matter_is_conserved = Judgment(matter, conserved)
-        self.assertTrue(matter.contains(Concept("presence")))
+        self.assertTrue(matter.contains(Concept("presence", True)))
         self.assertTrue(all_matter_is_conserved.is_synthetic())
 
